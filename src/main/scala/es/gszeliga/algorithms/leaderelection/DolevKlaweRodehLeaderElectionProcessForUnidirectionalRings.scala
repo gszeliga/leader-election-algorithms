9821505fb3b38f2f9b07fa3a3457ff05f9e2fa52
guillermo <gszeliga@gmail.com>
2016-04-19 00:37:31 +0200

    Dolev, Klawe, and Rodeh’s election algorithm implementation done

--- src/main/scala/es/gszeliga/algorithms/leaderelection/DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings.scala
+++ src/main/scala/es/gszeliga/algorithms/leaderelection/DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings.scala
@@ -11,18 +11,18 @@ import DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings._
 //Cost: O(n log n)
 object DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings {
 
-  type ID[V] = Comparable[V]
+  type ID[V] = Ordered[V]
 
   case class Config(next: ActorRef)
   case class Start()
   case class Election[V](round: Int, id: ID[V])
-  case class Elected[V](id1: ID[V], id2: ID[V])
+  case class Elected[V](whoIsElected: ID[V], whoEmitted: ID[V])
 
-  def props[V](pid: ID[V]) = Props(new DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](pid))
+  def props[V](pid: ID[V])(implicit ordering: Ordering[ID[V]]) = Props(new DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](pid))
 
 }
 
-class DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](val myself: ID[V]) extends Actor with ActorLogging{
+class DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](val myself: ID[V])(implicit val ordering: Ordering[ID[V]]) extends Actor with ActorLogging{
 
   private var next = Option.empty[ActorRef]
 
@@ -33,7 +33,7 @@ class DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](val myself: 
   private var competitor = false
 
   /*
-  * Greatest identity know by Pi
+  * Greatest identity known by Pi
   * */
   private var maxid = myself
 
@@ -41,6 +41,10 @@ class DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](val myself: 
   * Identity of the process for which Pi is competing
   * */
   private var proxy_for = Option.empty[ID[V]]
+
+  private var leader = Option.empty[ID[V]]
+  private var done = false
+  private var elected = false
 
   def receive = {
     case Config(ref) => next = Option(ref)
@@ -66,6 +70,42 @@ class DolevKlaweRodehLeaderElectionProcessForUnidirectionalRings[V](val myself: 
         //Incoming message has made a full turn on the ring, and consequently 'maxid' is the greatest identity
         next foreach (_ ! Elected(id, myself))
       }
+    }
+
+    case m @ Election(2,id) => {
+
+      def max(v1: ID[V], v2: ID[V]): ID[V] = {
+
+        if(v1 > v2) v1
+        else if(v1 < v2) v2
+        else v1
+      }
+
+      //Forward the message if we're no longer a competitor
+      if(!competitor) next foreach (_ ! m)
+      else if(proxy_for exists (_ > max(id.asInstanceOf[ID[V]], maxid))) {
+
+        //Update 'maxid' and start a new round
+        proxy_for foreach (proxy_ref => {
+          maxid = proxy_ref
+          next foreach (_ ! Election(1, proxy_ref))
+        })
+
+      }
+        //Since 'proxy_for' is not the highest identity, we stop competing
+      else competitor = false
+
+    }
+
+    case m @ Elected(whoIsElected,whoEmitted) => {
+
+      leader = Option(whoIsElected.asInstanceOf[ID[V]])
+      done = true
+      elected = whoIsElected == myself
+
+      //If not the one who emitted the elected message (meaning it didn't make a full turn)
+      if(whoEmitted != myself) next foreach(_ ! m)
+
     }
   }
 }
